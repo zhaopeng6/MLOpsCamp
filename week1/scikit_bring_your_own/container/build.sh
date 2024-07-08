e05c2dff85ef332030f33db2e769680446c1ae96
peng zhao <pengzhao@pengs-MacBook-Air.local>
2024-07-08 15:37:04 -0700

    first commit

---
+++ week1/scikit_bring_your_own/container/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This script shows how to build the Docker image and push it to ECR to be ready for use
+# by SageMaker.
+
+# The argument to this script is the image name. This will be used as the image on the local
+# machine and combined with the account and region to form the repository name for ECR.
+image=$1
+
+if [ "$image" == "" ]
+then
+    echo "Usage: $0 <image-name>"
+    exit 1
+fi
+
+chmod +x decision_trees/train
+chmod +x decision_trees/serve
+
+docker build  -t ${image} .
