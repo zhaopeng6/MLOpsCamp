32b8fa20a44992be101f36cb9cc22a09aec06ef1
yajing <jennyzhizhi@gmail.com>
2025-01-22 19:21:09 -0500

    update ecr to my aws account

--- week3/cicd/iris_docker/push_ecr.sh
+++ week3/cicd/iris_docker/push_ecr.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 ## Complete the following steps to get Docker image pushed to aws ecr locally
-region="us-west-1"
-ecr_repo="798223350085.dkr.ecr.us-west-1.amazonaws.com"
-image_name="iris_pred"
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker image pushed to aws ecr locally
+region="us-east-1"
+ecr_repo="977098985723.dkr.ecr.us-east-1.amazonaws.com"
+image_name="iris_predict_yl"
 image_tag="latest"
-echo Logging in to Amazon ECR...
+echo "Logging in to Amazon ECR..."
 aws --version
 aws ecr get-login-password --region $region | docker login --username AWS --password-stdin $ecr_repo
-docker build --tag=$image_name .
-docker tag $image_name:$image_tag $ecr_repo/$image_name:$image_tag
-docker push $ecr_repo/$image_name:$image_tag
+#docker build --tag=$image_name .
+#docker tag $image_name:$image_tag $ecr_repo/$image_name:$image_tag
+#docker push $ecr_repo/$image_name:$image_tag
+
