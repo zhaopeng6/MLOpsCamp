03e40f99804c2623ef50c0ab857ed11012a36794
yajing <jennyzhizhi@gmail.com>
2025-01-22 19:17:27 -0500

    rename docker image

--- week3/cicd/iris_docker/run_docker.sh
+++ week3/cicd/iris_docker/run_docker.sh
@@ -4,8 +4,8 @@
 
 # Step 1:
 # Build image
-docker build --tag=iris_pred .
+docker build --tag=iris_pred_yl .
 
 # Step 2:
 # Run flask app
-docker run -p 8000:8000 iris_pred
+docker run -p 8000:8000 iris_pred_yl
